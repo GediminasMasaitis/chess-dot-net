171afaa9f18e39a02cee7d35b573f6999c31582f
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2016-07-15 02:30:25 +0300

    Added a game class that will be used to control the engine

---
+++ ChessDotNet/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessDotNet.Data;
+using ChessDotNet.Evaluation;
+using ChessDotNet.MoveGeneration;
+using ChessDotNet.Searching;
+
+namespace ChessDotNet
+{
+    public class Game
+    {
+        private HyperbolaQuintessence Hyperbola { get; set; }
+        private EvaluationService Evaluation { get; set; }
+        private AttacksService Attacks { get; set; }
+        private PossibleMovesService Moves { get; set; }
+        private SearchService Search { get; set; }
+
+        public Game(string fen = null)
+        {
+            fen = fen ?? "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"; // Starting pos
+
+            var fact = new BoardFactory();
+            var board = fact.ParseFEN(fen);
+
+            var hyperbola = new HyperbolaQuintessence();
+            var evaluationService = new EvaluationService();
+            var attacksService = new AttacksService(hyperbola);
+            var movesService = new PossibleMovesService(attacksService, hyperbola);
+            var searchService = new SearchService(movesService, evaluationService);
+
+            Hyperbola = hyperbola;
+            Evaluation = evaluationService;
+            Attacks = attacksService;
+            Moves = movesService;
+            Search = searchService;
+        }
+
+    }
+}
